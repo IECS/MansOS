c9f7e3756289765c8a2c6e84477f6e531cea4a50
atis.elsts <atis.elsts@gmail.com>
2011-11-30 22:52:09 +0000

    added listening button

--- tools/sadlang_GUI/myAPP.py
+++ tools/sadlang_GUI/myAPP.py
@@ -1,6 +1,7 @@
 import wx
 from src import tabManager
 from src import uploadModule
+from src import listenModule
 from src import APIcore 
 
 class Example(wx.Frame):
@@ -17,12 +18,14 @@ class Example(wx.Frame):
         fileMenu = wx.Menu()
         save = fileMenu.Append(wx.ID_SAVE, '&Save\tCtrl+S', 'Save generated code')
         upload = fileMenu.Append(wx.ID_ANY, '&Upload\tCtrl+U', 'Upload generated code')
+        output = fileMenu.Append(wx.ID_ANY, '&Output\tCtrl+O', 'Listen to mote\'s output')
         close = fileMenu.Append(wx.ID_EXIT, '&Quit\tCtrl+Q', 'Quit application')
         menubar.Append(fileMenu, '&File')
         self.SetMenuBar(menubar)
         self.toolbar = self.CreateToolBar()
         extTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Quit', wx.Bitmap('src/exit.jpeg'))
         uplTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Upload', wx.Bitmap('src/upload.gif'))
+        outputTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Output', wx.Bitmap('src/output.gif'))
         self.toolbar.Realize()
         
         self.Bind(wx.EVT_MENU, self.OnQuit, close)
@@ -30,6 +33,8 @@ class Example(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnSave, save)
         self.Bind(wx.EVT_MENU, self.OnUpload, upload)
         self.Bind(wx.EVT_TOOL, self.OnUpload, uplTool)
+        self.Bind(wx.EVT_MENU, self.OnOutput, output)
+        self.Bind(wx.EVT_TOOL, self.OnOutput, outputTool)
         
     def OnQuit(self, e):
         self.Close()
@@ -38,7 +43,12 @@ class Example(wx.Frame):
         print "Save not suported :("
         
     def OnUpload(self, e):
-        dialog = uploadModule.UploadModule(None, 'Upload and Compile', self.API)
+        dialog = uploadModule.UploadModule(None, 'Upload and compile', self.API)
+        dialog.ShowModal()
+        dialog.Destroy()
+
+    def OnOutput(self, e):
+        dialog = listenModule.ListenModule(None, 'Listen to output', self.API)
         dialog.ShowModal()
         dialog.Destroy()
 
