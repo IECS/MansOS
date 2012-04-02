d4a8034968a8aa3042a61cdc72d1a061458660ca
andrey.vihrov <andrey.vihrov@gmail.com>
2012-04-02 08:43:35 +0000

    Commit latest radio driver version

--- mos/chips/cc1101/cc1101.h
+++ mos/chips/cc1101/cc1101.h
@@ -30,9 +30,9 @@
 #define CC1101_MAX_PACKET_LEN  62
 #define CC1101_SYNC_WORD       0xAAAAU
 #define CC1101_DEFAULT_CHANNEL 0
-#define CC1101_TX_POWER        0x50 /* 0 dBm at 868 MHz */
-#define CC1101_TX_POWER_MIN    0x03
-#define CC1101_TX_POWER_MAX    0xC0
+#define CC1101_TXPOWER         0x50 /* 0 dBm at 868 MHz */
+#define CC1101_TXPOWER_MIN     0x03
+#define CC1101_TXPOWER_MAX     0xC0
 
 typedef void (*cc1101Callback_t)(void);
 
