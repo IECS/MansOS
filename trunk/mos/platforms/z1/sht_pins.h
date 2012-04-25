2260ceb8f2b83abc6601424af67a53e465a95aaa
atis.elsts@gmail.com <atis.elsts@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2012-04-25 14:20:56 +0000

    z1 platform code

---
+++ trunk/mos/platforms/z1/sht_pins.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2008-2012 the MansOS team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of  conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SHT_PINS_H
+#define SHT_PINS_H
+
+// Zolertia Z1 platform uses SHT11 humidity sensor
+
+#define SHT11_SDA_PORT 2
+#define SHT11_SDA_PIN  3
+#define SHT11_SCL_PORT 2
+#define SHT11_SCL_PIN  4
+#define SHT11_PWR_PORT 1
+#define SHT11_PWR_PIN  7
+
+#endif
