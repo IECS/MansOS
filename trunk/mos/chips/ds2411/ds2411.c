ee7cade6e2a89d6d0cadb59b70a573dcf27ae129
andrey.vihrov@gmail.com <andrey.vihrov@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2012-06-18 09:27:55 +0000

    Move 1-Wire driver to separate file

--- trunk/mos/chips/ds2411/ds2411.c
+++ trunk/mos/chips/ds2411/ds2411.c
@@ -22,13 +22,14 @@
  */
 
 #include "ds2411.h"
-#include <hil/udelay.h>
-#include <hil/gpio.h>
-#include <lib/codec/crc.h>
 #include <string.h>
 #include "platform.h"
 
 #if SNUM_CHIP == SNUM_DS2411
+
+#define ONEWIRE_PORT DS2411_PORT
+#define ONEWIRE_PIN  DS2411_PIN
+#include <hil/onewire.h>
 
 static uint8_t ds2411[SERIAL_NUMBER_SIZE];
 static bool ds2411Ok;
@@ -43,121 +44,9 @@ bool ds2411SnumMatches(const uint8_t *snum) {
     return ds2411Ok && memcmp(ds2411, snum, SERIAL_NUMBER_SIZE) == 0;
 }
 
-/*
- * Set 1-Wire low or high.
- *
- * IMPORTANT: The GPIO pin must have a pullup resistor attached, either
- * internal or external.
- */
-#define OUTP_0() ( \
-    pinAsOutput(DS2411_PORT, DS2411_PIN), /* Enable master drive */ \
-    pinClear(DS2411_PORT, DS2411_PIN))    /* Output 0 */
-#define OUTP_1() ( \
-    pinAsInput(DS2411_PORT, DS2411_PIN),  /* Disable master drive */ \
-    pinSet(DS2411_PORT, DS2411_PIN))      /* Turn on pullup resistor, if any */
-
-/* Read one bit. */
-#define INP() pinRead(DS2411_PORT, DS2411_PIN)
-
-/*
- * Recommended delay times in us.
- */
-#define udelay_tA() udelay_6()
-/*      tA 6               max 15 */
-#define tB 64
-#define tC 60           /* max 120 */
-#define tD 10
-#define tE 9            /* max 12 */
-#define tF 55
-#define tG 0
-#define tH 480
-#define tI 70
-#define tJ 410
-
-static int
-owreset(void)
-{
-    int result;
-    OUTP_0();
-    udelay(tH);
-    OUTP_1();           /* Releases the bus */
-    udelay(tI);
-    result = INP();
-    udelay(tJ);
-    return result;
-}
-
-static void
-owwriteb(unsigned byte)
-{
-    int i = 7;
-    do {
-        if (byte & 0x01) {
-            OUTP_0();
-            udelay_tA();
-            OUTP_1();           /* Releases the bus */
-            udelay(tB);
-        } else {
-            OUTP_0();
-            udelay(tC);
-            OUTP_1();           /* Releases the bus */
-            udelay(tD);
-        }
-        if (i == 0)
-            return;
-        i--;
-        byte >>= 1;
-    } while (1);
-}
-
-static unsigned
-owreadb(void)
-{
-    unsigned result = 0;
-    int i = 7;
-    do {
-        OUTP_0();
-        udelay_tA();
-        OUTP_1();           /* Releases the bus */
-        udelay(tE);
-        if (INP())
-            result |= 0x80;     /* LSbit first */
-        udelay(tF);
-        if (i == 0)
-            return result;
-        i--;
-        result >>= 1;
-    } while (1);
-}
-
 int ds2411Init() {
-    unsigned family, crc, acc;
-    uint_t i;
-    uint8_t buffer[SERIAL_NUMBER_SIZE];
+    ds2411Ok = owReadROM(ds2411) && ds2411[7] == 0x01; // 0x01 is the family code
 
-    if (owreset()) return 0; // fail
-
-    Handle_t h;
-    ATOMIC_START(h);
-    owwriteb(0x33);     /* Read ROM command. */
-    family = owreadb();
-    /* We receive 6 bytes in the reverse order, LSbyte first. */
-    for (i = 7; i >= 2; i--) {
-        buffer[i] = owreadb();
-    }
-    crc = owreadb();
-    ATOMIC_END(h);
-
-    if (family != 0x01) return 0; // fail
-
-    acc = crc8Add(0x0, family);
-    for (i = 7; i >= 2; i--) {
-        acc = crc8Add(acc, buffer[i]);
-    }
-    if (acc != crc) return 0; // fail
-
-    ds2411Ok = true;
-    memcpy(ds2411, buffer, SERIAL_NUMBER_SIZE);
 #ifdef PLATFORM_TELOSB
     // 00:12:75    Moteiv    # Moteiv Corporation
     ds2411[0] = 0x00;
@@ -165,7 +54,7 @@ int ds2411Init() {
     ds2411[2] = 0x75;
 #endif
 
-    return 1;
+    return ds2411Ok;
 }
 
 #endif // SNUM_CHIP == SNUM_DS2411
