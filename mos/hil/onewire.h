6485fabdd200bfad3eb6092c236158f6b5fab4b0
andrey.vihrov <andrey.vihrov@gmail.com>
2012-06-18 09:27:55 +0000

    Move 1-Wire driver to separate file

---
+++ mos/hil/onewire.h
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2008-2012 the MansOS team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of  conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Generic 1-Wire driver
+ *
+ * Define ONEWIRE_PORT and ONEWIRE_PIN before including this file.
+ */
+
+#include <hil/udelay.h>
+#include <hil/gpio.h>
+#include <lib/codec/crc.h>
+
+#define ONEWIRE_ROM_SIZE 8
+
+/*
+ * Set 1-Wire low or high.
+ *
+ * IMPORTANT: The GPIO pin must have a pullup resistor attached, either
+ * internal or external.
+ */
+#define OUTP_0() ( \
+    pinAsOutput(DS2411_PORT, DS2411_PIN), /* Enable master drive */     \
+    pinClear(DS2411_PORT, DS2411_PIN))    /* Output 0 */
+#define OUTP_1() ( \
+    pinAsInput(DS2411_PORT, DS2411_PIN),  /* Disable master drive */    \
+    pinSet(DS2411_PORT, DS2411_PIN))      /* Turn on internal MSP430 */ \
+                                          /* pullup resistor, if any */
+
+/* Read one bit. */
+#define INP() pinRead(DS2411_PORT, DS2411_PIN)
+
+/*
+ * Recommended delay times in us.
+ */
+#define udelay_tA() udelay_6()
+/*      tA 6               max 15 */
+#define tB 64
+#define tC 60           /* max 120 */
+#define tD 10
+#define tE 9            /* max 12 */
+#define tF 55
+#define tG 0
+#define tH 480
+#define tI 70
+#define tJ 410
+
+static int
+owreset(void)
+{
+    int result;
+    OUTP_0();
+    udelay(tH);
+    OUTP_1();           /* Releases the bus */
+    udelay(tI);
+    result = INP();
+    udelay(tJ);
+    return result;
+}
+
+static void
+owwriteb(unsigned byte)
+{
+    int i = 7;
+    do {
+        if (byte & 0x01) {
+            OUTP_0();
+            udelay_tA();
+            OUTP_1();           /* Releases the bus */
+            udelay(tB);
+        } else {
+            OUTP_0();
+            udelay(tC);
+            OUTP_1();           /* Releases the bus */
+            udelay(tD);
+        }
+        if (i == 0)
+            return;
+        i--;
+        byte >>= 1;
+    } while (1);
+}
+
+static unsigned
+owreadb(void)
+{
+    unsigned result = 0;
+    int i = 7;
+    do {
+        OUTP_0();
+        udelay_tA();
+        OUTP_1();           /* Releases the bus */
+        udelay(tE);
+        if (INP())
+            result |= 0x80;     /* LSbit first */
+        udelay(tF);
+        if (i == 0)
+            return result;
+        i--;
+        result >>= 1;
+    } while (1);
+}
+
+static bool owReadROM(uint8_t buf[ONEWIRE_ROM_SIZE])
+{
+    int_t    i;
+    unsigned acc;
+
+    if (owreset()) return false; // fail
+
+    Handle_t h;
+    ATOMIC_START(h);
+    owwriteb(0x33);     /* Read ROM command. */
+    /* We receive 8 bytes in the reverse order, LSbyte first. */
+    for (i = 7; i >= 0; i--) {
+        buf[i] = owreadb();
+    }
+    ATOMIC_END(h);
+
+    acc = 0;
+    for (i = 7; i >= 1; i--) {
+        acc = crc8Add(acc, buf[i]);
+    }
+    return acc == buf[0];
+}
