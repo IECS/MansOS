0206dc5f0fa8303392eb7c014d1126b558aa8607
atis.elsts <atis.elsts@gmail.com>
2012-10-17 16:09:46 +0000

    SEAL: add copyrights

--- tools/seal/components/component_hierarchy.py
+++ tools/seal/components/component_hierarchy.py
@@ -1,3 +1,25 @@
+#
+# Copyright (c) 2012 Atis Elsts
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of  conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 TYPE_ACTUATOR = 1
 TYPE_SENSOR = 2
 TYPE_OUTPUT = 3
@@ -15,7 +37,7 @@ class SealAdvancedParameter(SealParameter):
     def __init__(self, value, valueList = []):
         super(SealAdvancedParameter, self).__init__(value, valueList)
         self.isAdvanced = True
-       
+
 #######################################################
 class SealComponent(object):
     def __init__(self, typeCode, name):
