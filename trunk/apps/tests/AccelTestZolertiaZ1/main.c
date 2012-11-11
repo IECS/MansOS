2a02674399b6298c9fef339e7a717511b455a701
girts.strazdins@gmail.com <girts.strazdins@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2012-11-11 15:03:03 +0000

    +) Zolertia Accel test app added;

---
+++ trunk/apps/tests/AccelTestZolertiaZ1/main.c
@@ -0,0 +1,52 @@
+#include <stdmansos.h>
+
+#define MAIN_LOOP_LENGTH 10 // ms
+
+#define START_DELAY    1000
+
+//
+// Sample 3D Accelerometer on Zolertia Z1 platform, print output to serial
+//
+typedef struct Packet {
+    uint32_t timestamp;
+    uint16_t acc_x;
+    uint16_t acc_y;
+    uint16_t acc_z;
+} Packet_t;
+
+static inline void printPacket(Packet_t *p) {
+    PRINTF("%lu %u %u %u\n", p->timestamp,
+            p->acc_x, p->acc_y, p->acc_z);
+}
+
+//-------------------------------------------
+//      Entry point for the application
+//-------------------------------------------
+void appMain(void)
+{
+    PRINT_INIT(255);
+
+    PRINTF("\n\nAccelerometer API test app\n");
+
+    accelOn();
+
+    mdelay(START_DELAY);
+
+    redLedOn();
+
+    for (;;) {
+        uint32_t now = getRealTime();
+        uint32_t endTime = now + MAIN_LOOP_LENGTH;
+
+        Packet_t p;
+        p.timestamp = now;
+
+        p.acc_x = accelReadX();
+        p.acc_y = accelReadY();
+        p.acc_z = accelReadZ();;
+
+        printPacket(&p);
+
+        while (timeAfter32(endTime, getRealTime()));
+    }
+}
