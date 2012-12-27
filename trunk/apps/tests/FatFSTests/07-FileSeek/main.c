15d87de12d7fb003c80d653dac74dd78999b9926
atis.elsts@gmail.com <atis.elsts@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2012-12-27 09:50:12 +0000

    update FAT fs test progs

---
+++ trunk/apps/tests/FatFSTests/07-FileSeek/main.c
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2012 the MansOS team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of  conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "stdmansos.h"
+#include <lib/assert.h>
+
+// test read/write in multiple clusters 
+#define NUM_TESTS 1441
+#define DATA_LEN 13
+
+void appMain(void)
+{
+    FILE *f = fopen("hello.txt", "w");
+    ASSERT(f);
+    ASSERT(f->fd != -1);
+
+    char buffer[DATA_LEN] = "0123456789abcdef";
+    char readBuffer[DATA_LEN];
+    int len;
+
+    uint16_t i;
+    for (i = 0; i < NUM_TESTS; ++i) {
+        len = fwrite(buffer, 1, DATA_LEN, f);
+        ASSERT(len == DATA_LEN);
+    }
+    fseek(f, 1001 * DATA_LEN, SEEK_SET);
+    len = fread(readBuffer, 1, DATA_LEN, f);
+    ASSERT(len == DATA_LEN);
+    ASSERT(!memcmp(readBuffer, buffer, DATA_LEN));
+
+    // close and reopen the file for reading
+    fclose(f);
+    memset(f, 0, sizeof(*f));
+    f = fopen("hello.txt", "r");
+    ASSERT(f);
+    ASSERT(f->fd != -1);
+
+    fseek(f, 1001 * DATA_LEN, SEEK_SET);
+    len = fread(readBuffer, 1, DATA_LEN, f);
+    ASSERT(len == DATA_LEN);
+    ASSERT(!memcmp(readBuffer, buffer, DATA_LEN));
+
+    fclose(f);
+    ASSERT(f->fd == -1);
+
+    PRINTF("done!\n");
+}
