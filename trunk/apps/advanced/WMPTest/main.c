ec79809c548a940e18f169bffae8fa55b57905ce
atis.elsts@gmail.com <atis.elsts@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2013-01-08 17:21:32 +0000

    WMP is now part of MansOS

--- trunk/apps/advanced/WMPTest/main.c
+++ trunk/apps/advanced/WMPTest/main.c
@@ -1,22 +1,7 @@
 #include "stdmansos.h"
-#include "wmp.h"
 
 void appMain(void)
 {
-//    humidityOn();
-
-//     while (1) {
-//         uint16_t light = lightRead();
-// //        uint16_t humidity = humidityRead();
-// //        uint16_t temperature = temperatureRead();
-//         PRINTF("light=%u\n", light);
-// //        PRINTF("humidity = %u\n", humidity);
-// //        PRINTF("temperature = %u\n", temperature);
-// //        msleep(2000); // sleep two seconds
-// msleep(200);
-//     }
-    wmpInit();
-
     for (;;) {
         PRINTF(".\n");
         mdelay(1000);
