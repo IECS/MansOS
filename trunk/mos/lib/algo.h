ca4fa4ea356694e67e6fcfe5988fc5b19bdd79d9
atis.elsts@gmail.com <atis.elsts@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2013-02-07 16:37:06 +0000

    fixes and improvements

--- trunk/mos/lib/algo.h
+++ trunk/mos/lib/algo.h
@@ -85,13 +85,12 @@ static inline int32_t map(int32_t value,
 //! Convert half-a-byte (so called "nibble") to a hexadecimal character
 static inline char toHex(uint8_t nibble) {
     switch (nibble) {
-    case 0 .. 9:
+    case 0 ... 9:
         return '0' + nibble;
-    case 10 .. 15:
+    case 10 ... 15:
     default:
         return 'a' + nibble - 10;
     }
 }
-
 
 #endif
