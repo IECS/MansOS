0e050da838d2281275f8038d54174b5fdeef48ec
janis.judvaitis@gmail.com <janis.judvaitis@gmail.com@e817e449-0fee-859b-2f65-e84856a30a99>
2013-04-30 09:19:56 +0000

    Add sm2 app used on deployment @ 24.04.

---
+++ trunk/apps/advanced/Sad2013/common.h
@@ -0,0 +1,30 @@
+#ifndef SAD_COMMON_H
+#define SAD_COMMON_H
+
+#include "stdmansos.h"
+#include <string.h>
+#include <net/socket.h>
+#include <net/routing.h>
+#include <lib/codec/crc.h>
+
+#define MANSOS
+#include "data_packet.h"
+
+#define DATA_PORT        123
+//#define TIME_SYNC_PORT 124
+
+// #define DATA_INTERVAL_JIFFIES      (5ul * 60 * HZ)
+// #define TIME_SYNC_INTERVAL_JIFFIES (5ul * 60 * HZ)
+//#define DATA_INTERVAL_JIFFIES         (HZ)
+//#define TIME_SYNC_INTERVAL_JIFFIES  (5ul * HZ)
+
+#define DATA_INTERVAL (1000ul * 60 * 15)  // -- once in every 15 minutes
+//#define DATA_INTERVAL       (5000ul)
+#define DATA_INTERVAL_SMALL (1000ul * 10)
+
+#define EXT_FLASH_RESERVED  (256 * 1024ul) // 256kb
+
+// #define ADS_CONVERSION_REGISTER 0
+// #define ADS_CONFIG_REGISTER 1
+
+#endif
