2a14789f40942bf3a339af8981e21a1ee163632d
andrey.vihrov <andrey.vihrov@gmail.com>
2013-07-09 08:52:46 +0000

    It seems miimote UART needs pin 1.0 set, after all

--- mos/platforms/miimote/platform.c
+++ mos/platforms/miimote/platform.c
@@ -42,7 +42,7 @@ void initPlatform(void)
 
     // Switch the UART/SDA-12 line to UART mode
     pinAsOutput(1, 0);
-    pinClear(1, 0);
+    pinSet(1, 0);
 
 #if USE_ADC
     // TODO: 1.5V or 2.5V ?
