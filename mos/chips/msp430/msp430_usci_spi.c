3749a5733135a2fd9aa077c334b57bbb93fdccae
atis.elsts <atis.elsts@gmail.com>
2013-11-08 14:32:04 +0000

    revert Andriev's change in SPI clock speed

--- mos/chips/msp430/msp430_usci_spi.c
+++ mos/chips/msp430/msp430_usci_spi.c
@@ -31,7 +31,7 @@
 
 // SPI mode: master, MSB first, 8-bit, 3-pin
 #define SPI_MODE  (UCCKPH | UCMSB | UCMST | UCMODE_0 | UCSYNC)
-#define SPI_SPEED (CPU_HZ )
+#define SPI_SPEED (CPU_HZ / 2)
 //#define SPI_SPEED 100000ul
 
 #define SETUP_SPI_PINS(letterid)                                            \
